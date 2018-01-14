8a83febf0283b7bf57b5658f88a73074938d0ff3
yudezhen <yudezhen@smzdm.com>
2018-01-14 22:40:17 +0800

    第一版提交

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <bing/>
+        <ForList/>
+        <!-- <HelloWorld/> -->
+    </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+import bing from './components/bing';
+import ForList from './components/forList';
+
+export default {
+    name: 'app',
+    components: {
+        bing, ForList
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/forList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ul class="list">
+            <li v-for='item in forItem'>
+               <template>
+                    <span v-for='(value, key, index) in item'>{{value}}----{{key}}------{{index}}</span>
+                </template>
+            </li>
+        </ul>
+        <ol class="list">
+            <li v-for='(value, key, index) in obj'>
+                {{value}}----{{key}}------{{index}}
+            </li>
+        </ol>
+        <h3>多选按钮绑定一个值</h3>
+        <input type="checkbox" id="isTrue" v-model='isTrue'>
+        <label for="isTrue">{{isTrue}}</label>     
+    </div>
+</template>
+
+<script type='text/javascript'>
+    export default{
+        name: 'forList',
+        data () {
+            return {
+                forItem: [
+                    {'name': 'yutou'},
+                    {'name': 'chenlin'},
+                    {'name': 'lili'}
+                ],
+                obj: {
+                    firstName: 'JOND',
+                    LastName: 'ddg',
+                    age: 30
+                },
+                isTrue: true
+            };
+        }
+    };
+</script>
